067f446712b7aa68e1ad319662da025cfefbbccd
Kaushik Nag Tumu <138563218+404kaushik@users.noreply.github.com>
2024-08-22 14:51:17 -0400

    Removed Backend

---
+++ src/components/Header.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import logo from '../assets/logo2.png';
+
+const navItems = [
+  { label: 'About', href: '#apply' },
+  { label: 'Applications', href: '#applications' },
+  { label: 'Sponsors', href: '#' },
+  { label: 'FAQ', href: '#' },
+  { label: 'The Team', href: '#' },
+  { label: 'Contact Us', href: '#' },
+];
+
+const Header = () => {
+  const [navIsHidden, setNavIsHidden] = useState(true);
+
+  const toggleNav = () => {
+    setNavIsHidden(!navIsHidden);
+  };
+
+  // Minimize nav when window is smaller than 1024px
+  const handleResize = () => {
+    if (window.innerWidth < 1024) {
+      setNavIsHidden(true);
+    } else {
+      setNavIsHidden(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <nav className="bg-[#1C1D21] dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b-2 border-gray-200 dark:border-gray-600">
+      <div className="w-screen flex flex-wrap items-center justify-between mx-auto p-6 box-border">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={logo} className="h-16 object-contain" alt="HackTrent Logo" />
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            onClick={toggleNav}
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`${navIsHidden ? 'hidden' : ''} items-center justify-between w-full md:flex md:w-auto md:order-1`}
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg bg-[#1C1D21] md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 font-varela">
+            {navItems.map((item) => (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="block py-2 px-3 text-[#F9F5E3] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  aria-current="page"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}          
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import text from '../assets/text.png';
+import CountDown from './CountDown';
+import Stars from './Stars';
+import '../App.css';
+const Home = () => {
+  return (
+    <div className="p-4">
+      <Stars starCount={1000} />
+      {/* <div className="flex items-end justify-end">
+        <img src={planet1} alt="" className='absolute inset-0 -translate-y-36 w-[492px] h-[919px]'/>
+      </div> */}
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <div className="flex flex-col items-center justify-center mt-60 mb-40 md:mb-60 relative">
+          <div className="sm:w-[85%] md:w-[80%] ">
+            <img src={text} alt="" className='w-full'/>
+          </div>
+          <div className="sm:w-[95%] md:w-[100%] lg:w-[75%] text-center">
+            <span className='text-5xl sm:text-8xl font-potta-one font-normal leading-none text-[#f9f5e3] animate-bounce'>
+              November 8&#8209;10
+            </span>
+          </div>
+          <button className='bg-[#36382E] my-8 px-9  h-24 rounded-[16px] text-2xl font-space-mono text-[#F9F5E3] transition ease-in-out delay-100 hover:font-bold hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-100'>
+            Apply as Hacker
+          </button>
+        </div>
+        <CountDown />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
