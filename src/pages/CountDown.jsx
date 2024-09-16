1f22f4585e47f89531ddd1bcfa49a3026b6df94b
Kaushik Nag Tumu <138563218+404kaushik@users.noreply.github.com>
2024-09-15 21:59:35 -0400

    Changed CountDown to match the exact Date

--- src/pages/CountDown.jsx
+++ src/pages/CountDown.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import About from './About';
 
 const CountDown = () => {
-  const targetDate = '2024-12-25T00:00:00'; // Set your target date here
+  const targetDate = '2024-11-08T00:00:00'; // Set your target date here
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(targetDate));
 
