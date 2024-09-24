f08b75cedf4b86964c97099e46272fade4d94ebb
Kaushik Nag Tumu <138563218+404kaushik@users.noreply.github.com>
2024-09-24 09:23:43 -0400

    Adding google analytics to the website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
@@ -8,6 +10,14 @@ import Contact from "./pages/Contact.jsx";
 import 'framer-motion';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Track page views on route change
+    window.gtag('config', 'G-X50BHTH4DP', {
+      page_path: location.pathname,
+    });
+  }, [location]);
 
   return (
     <div className="flex flex-col min-h-screen">
